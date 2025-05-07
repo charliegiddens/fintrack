4d37614bbad8b8a350a70421f77c95d186a3e4b4
Charlie Giddens <charlie@blackwater.gg>
2025-05-07 19:24:27 +0100

    Added API Helper function to assist conversion from Auth0 Subject ID to Internal user ID

---
+++ backend/app/api_helpers.py
@@ -0,0 +1,20 @@
+from models.user import User
+
+def get_internal_user_id_from_auth0_sub(auth0_subject_id: str):
+    """
+    Retrieves the local Fintrack user ID based on the Auth0 subject ID.
+
+    Args:
+        auth0_subject_id: The 'sub' claim from the Auth0 token.
+
+    Returns:
+        The integer Fintrack user ID if found, otherwise None.
+    """
+    if not auth0_subject_id:
+        return None
+
+    internal_user = User.query.filter_by(auth0_subject=auth0_subject_id).first()
+
+    if internal_user:
+        return internal_user.id
+    return None
