d12d5f2a97890f82268537bde81d916261675d29
Charlie Giddens <charlie@blackwater.gg>
2025-05-07 23:09:30 +0100

    Removed unused tests from old caching implementation

--- backend/tests/test_config.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app
-
-def test_development_config(app):
-    """Check if TestingConfig is loaded correctly."""
-
-    assert current_app.config['TESTING'] is True
-    assert current_app.config['SQLALCHEMY_DATABASE_URI'] == "sqlite:///:memory:"
-    assert current_app.config['SECRET_KEY'] == 'test_secretkey'
